0c15e1419eb17309f57a507c056904c1f872d57e
Chrispus <skilochris@gmail.com>
2023-10-27 16:51:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +16,29 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  function hasTwoElementsWithSum(array, target):
+    for i from 0 to (length of array - 1):
+        complement = target - array[i]
+        for j from (i + 1) to (length of array - 1):
+            if array[j] is equal to complement:
+                return true
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  iterate over the array of numbers 
+  to compare each element with the complement of the target 
+  and returns true if a pair of elements is found that sum up to the target, 
+  and false otherwise.
+  (for example: if our number is 5, and the target is 10, the complementary number is 5)
+
+
+
+
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
